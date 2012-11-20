2e927a313d75aeacad6bb8fbf48f5d37f4cd5a59
Chase Seibert <cseibert@hearsaycorp.com>
2012-11-20 11:57:36 -0800

    Updating long desc; throwing error on pip install

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='django-pyfixtures',
-    version='0.1',
+    version='0.1.1',
     author='Chase Seibert',
     author_email='chase.seibert@gmail.com',
     packages=['pyfixtures'],
+    url='https://github.com/chase-seibert/django-pyfixtures',
     download_url='https://github.com/chase-seibert/django-pyfixtures/tarball/master',
     license='LICENSE.txt',
     description='dumpdata generates python source code files as the fixtures',
-    long_description=open('README.md').read(),
 )
