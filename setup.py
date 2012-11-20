9c978127d7451259da0de80d713105b8d52b5524
Chase Seibert <cseibert@hearsaycorp.com>
2012-11-20 11:36:14 -0800

    Fixing pip install

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Chase Seibert',
     author_email='chase.seibert@gmail.com',
     packages=['pyfixtures'],
-    url='https://github.com/chase-seibert/pyfixtures',
+    url='https://github.com/chase-seibert/django-pyfixtures',
     license='LICENSE.txt',
     description='dumpdata generates python source code files as the fixtures',
     long_description=open('README.md').read(),
