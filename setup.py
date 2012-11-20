a15af991b328af88d9e115a086fd91e33c64f11d
Chase Seibert <cseibert@hearsaycorp.com>
2012-11-20 11:43:34 -0800

    Adding explicit download_url

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author_email='chase.seibert@gmail.com',
     packages=['pyfixtures'],
     url='https://github.com/chase-seibert/django-pyfixtures',
+    download_url='https://github.com/chase-seibert/django-pyfixtures/tarball/master',
     license='LICENSE.txt',
     description='dumpdata generates python source code files as the fixtures',
     long_description=open('README.md').read(),
